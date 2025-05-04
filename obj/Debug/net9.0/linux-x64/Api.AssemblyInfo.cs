bb5dbf0f6611cf631a929d3b40bbe6b44c40bb14
Lucas <lucas_avila_96@hotmail.com>
2025-05-04 17:11:36 -0300

    Add Visual Studio solution file and update assembly information

--- obj/Debug/net9.0/linux-x64/Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/linux-x64/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5551dbddcbf096251fd5dd527530657ccd9cf9d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf54980ec1e77e73b729360a1bc2d266fd5cdd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
