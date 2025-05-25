d29870376c4617d874a095db8c06e74de41ed9d5
Lucas <lucas_avila_96@hotmail.com>
2025-05-25 12:05:26 -0300

    Update JWT key in appsettings and enable CORS in Program.cs

--- Program.cs
+++ Program.cs
@@ -62,11 +62,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseCors();
 
 app.Run();
--- obj/Debug/net9.0/linux-x64/Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/linux-x64/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95a992f4a46b37e096708011666ea871be341ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19d0b2c5d5d78e5c3997aaedda967a854b854a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
