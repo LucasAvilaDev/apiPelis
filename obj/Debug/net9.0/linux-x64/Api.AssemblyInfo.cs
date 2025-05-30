679b8ad867d7dbc37d7a02ff51cf581b438d526b
Lucas <lucas_avila_96@hotmail.com>
2025-05-30 18:26:37 -0300

    Update assembly information and binaries for Api; regenerate source link

--- obj/Debug/net9.0/linux-x64/Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/linux-x64/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29870376c4617d874a095db8c06e74de41ed9d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e254c2f0efd68006455232185ee0e574d562ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
